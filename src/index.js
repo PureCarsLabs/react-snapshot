a70e45ccafa0186c71e9d4a5a2c774cb61045810
Joel Kanzelmeyer <joelk@purecars.com>
2019-12-06 16:38:17 -0500

    feat: add support for custom server render logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 
-export const render = (rootComponent, domElement) => {
+export const render = (rootComponent, domElement, serverRenderer) => {
   if (navigator.userAgent.match(/Node\.js/i) && window && window.reactSnapshotRender) {
-    domElement.innerHTML = ReactDOMServer.renderToString(rootComponent)
+    let html
+    if (serverRenderer) {
+      html = serverRenderer(ReactDOMServer.renderToString, rootComponent)
+    } else {
+      html = ReactDOMServer.renderToString(rootComponent)
+    }
+    domElement.innerHTML = html
     window.reactSnapshotRender()
   } else {
     ReactDOM.createRoot(domElement, { hydrate: true }).render(rootComponent)
